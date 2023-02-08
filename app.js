d2fd03fcadf101d1fdc04696b7e6b941986b835a
salAhamad <salman.ahmad@index.ae>
2023-02-08 16:29:26 +0400

    new changes for tones

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const root = document.querySelector('#root');
 const $inputHex = document.querySelector('#hexValue');
+const $inputHexParent = document.querySelector('.inputs-container__input--hex');
+const $rgbInputsParent = document.querySelector('.inputs-container__input--rgb');
 const $rgbInputs = document.querySelectorAll('.rgb-input');
 const $reverseButton = document.querySelector('.reverse-button');
 const $labelHEX = document.querySelector('.label-hex');
@@ -8,8 +10,26 @@ const $copyBlockRGB = document.querySelector('.copy-block-rgb');
 const $copyBlockHEX = document.querySelector('.copy-block-hex');
 
 let error_enabled = false;
-let hexValue = '#191919;';
+let hexValue = '#191919';
 let converted_rgb_value, color_brightness;
+
+let shades_length = 10;
+let dark_shades = [];
+
+
+// ============ [ Generate Random HEX Color ] ============ //
+function getRandomHexColor() {
+  return '0123456789ABCDEF'.split('').reduce((a, c, i, arr) => {
+    return i < 6 ? a + arr[Math.floor(Math.random() * 16)] : a;
+  }, '#');
+}
+
+// ============ [ Generate Random RGB Color ] ============ //
+function getRandomRgbColor() {
+  return Array(3)
+    .fill()
+    .map(() => Math.floor(Math.random() * 255));
+}
 
 // ============ [ RGB TO HEX FUNCTION ] ============ //
 const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('');
@@ -63,6 +83,10 @@ function rootColorsController(event) {
     root.style.setProperty('--base-color', '#e6e6e6');
     root.style.setProperty('--input-bg-color', 'transparent');
   };
+
+  if(hexValue === "#FB9A09") {
+    root.style.setProperty('--primary', '#051284');
+  } else root.style.setProperty('--primary', '#FB9A09');
 }
 
 // ============ [ slicing hex color for length with # ] ============ //
@@ -77,6 +101,17 @@ function sliceHexColor(color) {
 // ============ [ copy to clipboard ] ============ //
 function copyToClipboard(event) {
   navigator.clipboard.writeText(event);
+}
+
+// ============ [ create copy tooltip after click ] ============ //
+const coppeidTooltipCreation = (e, tooltipValue) => {
+  const coppiedElement = document.createElement('div');
+  coppiedElement.classList.add('coppeid-tooltip')
+  coppiedElement.innerHTML = tooltipValue || 'Coppied';
+  e.target.appendChild(coppiedElement);
+  setTimeout(() => {
+    e.target.removeChild(coppiedElement);
+  }, 1000)
 }
 
 // ============ [ RGB to HEX Conversion ] ============ //
@@ -94,7 +129,10 @@ $rgbInputs.forEach((input, index) => {
     let input_alert = thisInputParent.querySelector('.input-alert');
     let nextSibling = e.target.nextElementSibling;
 
-    
+  // Remove errors from other color inputs    
+    $inputHexParent.classList.remove('error');
+    $inputHexParent.querySelector('.input-alert').innerHTML = '';
+
 
     if(thisElem_value > 255) {
       input_alert.innerHTML = `Input value cannot be more than ${rgb_max_value}`;
@@ -115,9 +153,8 @@ $rgbInputs.forEach((input, index) => {
       if(thisElem.name === 'value-green') greenValue = thisElem_value
       if(thisElem.name === 'value-blue') blueValue = thisElem_value
       
-      console.log(redValue, greenValue, blueValue)
-
       let converted_rgb_to_hex = rgbToHex(+redValue, +greenValue, +blueValue);      
+      hexValue = `#${sliceHexColor(converted_rgb_to_hex)}`;
       $inputHex.value = sliceHexColor(converted_rgb_to_hex);
       color_brightness = lightOrDark(converted_rgb_to_hex);
       rootColorsController(color_brightness);
@@ -137,35 +174,36 @@ $inputHex.addEventListener('input', (e) => {
   let input_min_value = 3;
   
   let input_alert = thisInputParent.querySelector('.input-alert');
-  
-  if(input_type === 'input-hex') {
-    // checking if # is exists
-    if(input_value.includes('#')) {
-      input_min_value += 1
-      input_max_value += 1
-    };
 
-    // checking min-length and max-length for errors
-    if(input_value.length === input_min_value || input_value.length === input_max_value) {
-      input_alert.innerHTML = ''
-      input_alert.classList.add('display--none');
-      thisInputParent.classList.remove('error');
-      error_enabled = false;
-    } else {
-      input_alert.innerHTML = `Input Value cannot be less than ${input_min_value} and not more that ${input_max_value}`
-      input_alert.classList.remove('display--none');
-      thisInputParent.classList.add('error');
-      error_enabled = true;
-    }
+  // Remove errors from other color inputs
+  $rgbInputsParent.classList.remove('error');
+  $rgbInputsParent.querySelector('.input-alert').innerHTML = '';
+    
+  if(input_value.includes('#')) {
+    input_min_value += 1
+    input_max_value += 1
+  };
 
-    hexValue = input_value.includes('#') ? input_value : `#${input_value}`;    
-    const [ r, g, b ] = hexToRgb(input_value)   
-    $rgbInputs.forEach((input, index) => {
-      if(index === 0) input.value = r;
-      if(index === 1) input.value = g;
-      if(index === 2) input.value = b;
-    })
+  // checking min-length and max-length for errors
+  if(input_value.length === input_min_value || input_value.length === input_max_value) {
+    input_alert.innerHTML = ''
+    input_alert.classList.add('display--none');
+    thisInputParent.classList.remove('error');
+    error_enabled = false;
+  } else {
+    input_alert.innerHTML = `Input Value cannot be less than ${input_min_value} and not more that ${input_max_value}`
+    input_alert.classList.remove('display--none');
+    thisInputParent.classList.add('error');
+    error_enabled = true;
   }
+  
+  hexValue = input_value.includes('#') ? input_value : `#${input_value}`;
+  const [ r, g, b ] = hexToRgb(input_value)   
+  $rgbInputs.forEach((input, index) => {
+    if(index === 0) input.value = r;
+    if(index === 1) input.value = g;
+    if(index === 2) input.value = b;
+  })
 
   color_brightness = lightOrDark(hexValue);
   rootColorsController(color_brightness);
@@ -185,10 +223,63 @@ $reverseButton.addEventListener('click', (event) => {
   }
 });
 
-
-
+// ============ [ RGB : Coppied to Clipborad ] ============ //
 $copyBlockRGB.addEventListener('click', e => {
   let combinedRgbValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+  coppeidTooltipCreation(e);
   copyToClipboard(combinedRgbValue)
 });
-$copyBlockHEX.addEventListener('click', e => copyToClipboard(hexValue));
+
+// ============ [ HEX : Coppied to Clipborad ] ============ //
+$copyBlockHEX.addEventListener('click', e => {
+  coppeidTooltipCreation(e);
+  copyToClipboard(hexValue.toUpperCase())
+});
+
+// ============ [ Checking if primary color ] ============ //
+
+
+
+function adjustBrightness(col, amt) {
+  var usePound = false;
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  var R = parseInt(col.substring(0,2),16);
+  var G = parseInt(col.substring(2,4),16);
+  var B = parseInt(col.substring(4,6),16);
+
+  // to make the colour less bright than the input
+  // change the following three "+" symbols to "-"
+  R = R + amt;
+  G = G + amt;
+  B = B + amt;
+
+  if (R > 255) R = 255;
+  else if (R < 0) R = 0;
+
+  if (G > 255) G = 255;
+  else if (G < 0) G = 0;
+
+  if (B > 255) B = 255;
+  else if (B < 0) B = 0;
+  var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+  var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+  var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+  return (usePound?"#":"") + RR + GG + BB;
+}
+
+const shades_container = document.querySelector('.colors_shade');
+const generateShades = (color) => {
+  for (let i = 0; i < shades_length; i++) {
+    let bg_color = adjustBrightness(color, i);
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.style.backgroundColor = bg_color;
+    li.innerHTML = bg_color;
+    shades_container.appendChild(li);
+  }
+}
